9b1b64edd542a00d8d2595ff670672aa02b87b26
LinyuanZhang <lzz0035@auburn.edu>
2017-02-20 23:19:35 -0600

    Red Light |

--- softwareprocess/Sample.py
+++ softwareprocess/Sample.py
@@ -65,7 +65,8 @@ class Sample(object):
         return result
     
     def integrate(self, lowBound, highBound, n, f):
-    '''   epsilon = 0.001
+'''
+        epsilon = 0.001
         simpsonOld = 0.0
         simpsonNew = epsilon
         s = 4
