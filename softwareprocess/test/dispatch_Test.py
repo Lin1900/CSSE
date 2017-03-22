e81731212c50f7cc5a5fbcc8ae3766e613e03808
LinyuanZhang <lzz0035@auburn.edu>
2017-03-22 17:23:13 -0500

    Green Light |

--- softwareprocess/test/dispatch_Test.py
+++ softwareprocess/test/dispatch_Test.py
@@ -17,7 +17,8 @@ class DispatchTest(unittest.TestCase):
 
     def test_100_004(self):
         output = SD.dispatch({'observation': '100d46.4', 'op': 'adjust'})
-        self.assertTrue(output == {'observation': '100d46.4', 'height': '0', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '72', 'error': 'observation is invalid'})
+        inputVal= {'observation': '100d46.4', 'height': '0', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '72', 'error': 'observation is invalid'})
+        self.assertTrue(inputVal == output)
 
     def test_100_005(self):
         output = SD.dispatch({'op': 'adjust'})
