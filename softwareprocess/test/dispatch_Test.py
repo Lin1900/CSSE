fd3569fd7de256fb1566c1e3311bb1374350c958
LinyuanZhang <lzz0035@auburn.edu>
2017-03-22 10:46:22 -0500

    Red Light |

--- softwareprocess/test/dispatch_Test.py
+++ softwareprocess/test/dispatch_Test.py
@@ -5,9 +5,9 @@ class DispatchTest(unittest.TestCase):
     def test100_010_missinput(self):
         input = {}
         output = {'error': 'no op is specified'}
-        self.assertEqual(SD.(input), output)
+        self.assertEqual(SD(input), output)
 
     def test_dispatch100_110_givenCase1(self):
         input = {'observation': '30d1.5', 'height': '19.0', 'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
         output = {'altitude':'29d59.9', 'observation': '30d1.5', 'height': '19.0', 'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
-        self.assertEqual(SD.(input) == output)
+        self.assertEqual(SD(input) == output)
