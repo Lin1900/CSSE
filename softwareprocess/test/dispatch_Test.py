8f788e129bee49b4b8bea5377402afc55e879806
LinyuanZhang <lzz0035@auburn.edu>
2017-03-22 10:38:50 -0500

    Green Light |

--- softwareprocess/test/dispatch_Test.py
+++ softwareprocess/test/dispatch_Test.py
@@ -5,4 +5,9 @@ class DispatchTest(unittest.TestCase):
     def test100_010_missinput(self):
         input = {}
         output = {'error': 'no op is specified'}
-        self.assertEqual(SD.dispatch(input), output)
+        self.assertEqual(SD.(input), output)
+
+    def test_dispatch100_110_givenCase1(self):
+        inputVal = {'observation': '30d1.5', 'height': '19.0', 'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
+        output = {'observation': '30d1.5', 'height': '19.0', 'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
+        self.assertTrue(returnedValue == inputVal)
