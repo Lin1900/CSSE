168cfe75e5673ecf3c628df23a9f65b5bb165cd0
LinyuanZhang <lzz0035@auburn.edu>
2017-03-22 14:23:10 -0500

    Green Light |

--- softwareprocess/dispatchTest.py
+++ softwareprocess/dispatchTest.py
@@ -10,7 +10,7 @@ class DispatchTest(unittest.TestCase):
 
     def test_dispatch100_005_observationValueIllegal(self):
         input = {'op': 'adjust', 'observation': '45d123.4'}
-        returnedValue = SD.dispatch(inputVal)
+        returnedValue = SD.dispatch(input)
         input == {'error': 'minute is invalid'}
         self.assertTrue(returnedValue == input)
 
