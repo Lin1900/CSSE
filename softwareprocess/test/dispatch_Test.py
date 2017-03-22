a9e58c89aa7f3634fc1dfb94fdea52414464c5ab
LinyuanZhang <lzz0035@auburn.edu>
2017-03-22 15:08:18 -0500

    Green Light |

--- softwareprocess/test/dispatch_Test.py
+++ softwareprocess/test/dispatch_Test.py
@@ -19,8 +19,26 @@ class DispatchTest(unittest.TestCase):
         output = SD.dispatch({'op':'adjust', 'observation': '45d73.4'})
         self.assertTrue(output == {'error': 'minute is invalid'})
 
-    def test_dispatch100_110_givenCase1(self):
+    def test_200_001_givenCase1(self):
         input = {'observation': '30d1.5', 'height': '19.0', 'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
         inputVal = SD.dispatch(input)
         output = {'altitude':'29d59.9', 'observation': '30d1.5', 'height': '19.0', 'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
-        self.assertEqual(inputVal == output)
+        self.assertTrue(inputVal == output)
+
+    def test_200_002_givenCase1(self):
+        input = {'observation': '40d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
+        inputVal = SD.dispatch(input)
+        output = {'altitude':'45d11.9', 'observation': '30d1.5', 'height': '19.0', 'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
+        self.assertTrue(inputVal == output)
+
+    def test_200_003_givenCase1(self):
+        input = {'observation': '42d0.0', 'op': 'adjust', 'extraKey': 'ignore'}
+        inputVal = SD.dispatch(input)
+        output = {'altitude':'41d59.0', 'observation': '42d0.0', 'op': 'adjust', 'extraKey': 'ignore'}
+        self.assertTrue(inputVal == output)
+
+    def test_200_004_givenCase1(self):
+        input = {'observation': '101d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
+        inputVal = SD.dispatch(input)
+        output = {'observation': '101d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71', 'error': 'observation is invalid'}
+        self.assertTrue(inputVal == output)
