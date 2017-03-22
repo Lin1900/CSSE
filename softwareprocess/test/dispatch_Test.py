af33c5764b8cf8de9e46fda2fc5a3286697765cb
LinyuanZhang <lzz0035@auburn.edu>
2017-03-22 17:33:42 -0500

    Green Light |

--- softwareprocess/test/dispatch_Test.py
+++ softwareprocess/test/dispatch_Test.py
@@ -17,51 +17,57 @@ class DispatchTest(unittest.TestCase):
 
     def test_100_004(self):
         output = SD.dispatch({'op': 'adjust'})
-        self.assertTrue(output == {'error': 'mandatory information is missing'})
+        self.assertTrue(output == {'op': 'adjust', 'error': 'mandatory information is missing'})
 
     def test_100_005(self):
         output = SD.dispatch()
         self.assertTrue(output == {'error': 'parameter is missing'})
 
 
-    def test_200_001_givenCase1(self):
+    def test_200_001_validvalue(self):
         input = {'observation': '30d1.5', 'height': '19.0', 'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
         inputVal = SD.dispatch(input)
         output = {'altitude':'29d59.9', 'observation': '30d1.5', 'height': '19.0', 'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
         self.assertTrue(inputVal == output)
 
-    def test_200_002_givenCase1(self):
+    def test_200_002_validvalue(self):
         input = {'observation': '45d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
         inputVal = SD.dispatch(input)
         output = {'altitude':'45d11.9', 'observation': '45d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
         self.assertTrue(inputVal == output)
 
-    def test_200_003_givenCase1(self):
+    def test_200_003_validvalue(self):
         input = {'observation': '42d0.0', 'op': 'adjust', 'extraKey': 'ignore'}
         inputVal = SD.dispatch(input)
         output = {'altitude':'41d59.0', 'observation': '42d0.0', 'op': 'adjust', 'extraKey': 'ignore'}
         self.assertTrue(inputVal == output)
 
-    def test_300_001_givenCase1(self):
+    def test_300_001_invalidvalue(self):
         input = {'observation': '101d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
         inputVal = SD.dispatch(input)
         output = {'observation': '101d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71', 'error': 'observation is invalid'}
         self.assertTrue(inputVal == output)
 
-    def test_300_002_givenCase1(self):
+    def test_300_002_invalidvalue(self):
         input = {'observation': '45d15.2', 'height': 'a', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
         inputVal = SD.dispatch(input)
         output = {'observation': '45d15.2', 'height': 'a', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71', 'error': 'height is invalid'}
         self.assertTrue(inputVal == output)
 
-    def test_300_003_givenCase1(self):
+    def test_300_003_invalidvalue(self):
         input = {'observation': '45d15.2', 'height': '6', 'pressure': '1010', 'horizon': ' ', 'op': 'adjust', 'temperature': '71'}
         inputVal = SD.dispatch(input)
         output = {'observation': '45d15.2', 'height': '6', 'pressure': '1010', 'horizon': ' ', 'op': 'adjust', 'temperature': '71', 'error': 'horizon is invalid'}
         self.assertTrue(inputVal == output)
 
-    def test_300_004_givenCase1(self):
+    def test_300_004_invalidvalue(self):
         input = {'observation': '0d0.02', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
         inputVal = SD.dispatch(input)
         output = {'observation': '0d0.02', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71', 'error': 'observation is invalid'}
+        self.assertTrue(inputVal == output)
+
+    def test_300_005_invalidvalue(self):
+        input = {'observation': '45d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
+        inputVal = SD.dispatch(input)
+        output = {'observation': '45d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71', 'error': 'observation is invalid'}
         self.assertTrue(inputVal == output)
