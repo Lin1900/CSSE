29a38745ec515cd4b1ec82ec9392e406db65ce72
LinyuanZhang <lzz0035@auburn.edu>
2017-03-22 13:58:10 -0500

    Green Light |

---
+++ softwareprocess/dispatchTest.py
@@ -0,0 +1,9 @@
+import unittest
+import softwareprocess.dispatch as SD
+
+class DispatchTest(unittest.TestCase):
+    def test_dispatch100_000_emptyInput(self):
+        inputVal = {}
+        returnedValue = SD.dispatch(inputVal)
+        self.assertTrue(returnedValue == {'error': 'no op is specified'})
+
