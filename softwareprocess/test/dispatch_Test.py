ff0c7fab278caa7836dd887b03dea9d71c118610
LinyuanZhang <lzz0035@auburn.edu>
2017-03-22 15:25:32 -0500

    Green Light |

--- softwareprocess/test/dispatch_Test.py
+++ softwareprocess/test/dispatch_Test.py
@@ -19,6 +19,10 @@ class DispatchTest(unittest.TestCase):
         output = SD.dispatch({'op':'adjust', 'observation': '45d73.4'})
         self.assertTrue(output == {'error': 'minute is invalid'})
 
+    def test_100_005(self):
+        output = SD.dispatch({'op':'adjust', 'observation': '45d73.4'})
+        self.assertTrue(output == {'error': 'minute is invalid'})
+
     def test_200_001_givenCase1(self):
         input = {'observation': '30d1.5', 'height': '19.0', 'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
         inputVal = SD.dispatch(input)
@@ -28,7 +32,7 @@ class DispatchTest(unittest.TestCase):
     def test_200_002_givenCase1(self):
         input = {'observation': '45d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
         inputVal = SD.dispatch(input)
-        output = {'altitude':'45d11.9', 'observation': '30d1.5', 'height': '6', 'pressure': '1010', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
+        output = {'altitude':'45d11.9', 'observation': '45d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
         self.assertTrue(inputVal == output)
 
     def test_200_003_givenCase1(self):
@@ -38,6 +42,12 @@ class DispatchTest(unittest.TestCase):
         self.assertTrue(inputVal == output)
 
     def test_200_004_givenCase1(self):
+        input = {'observation': '101d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
+        inputVal = SD.dispatch(input)
+        output = {'observation': '101d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71', 'error': 'observation is invalid'}
+        self.assertTrue(inputVal == output)
+
+    def test_200_005_givenCase1(self):
         input = {'observation': '101d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
         inputVal = SD.dispatch(input)
         output = {'observation': '101d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71', 'error': 'observation is invalid'}
