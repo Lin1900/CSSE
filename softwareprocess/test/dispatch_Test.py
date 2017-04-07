384b1161241bac6120ae3145813456fd73558005
LinyuanZhang <lzz0035@auburn.edu>
2017-04-07 18:09:29 -0500

    Red Light |

--- softwareprocess/test/dispatch_Test.py
+++ softwareprocess/test/dispatch_Test.py
@@ -108,3 +108,7 @@ class DispatchTest(unittest.TestCase):
     def test_400_001(self):
         output = SD.dispatch({'op': 'predict','body': 'unknown', 'date': '2016-01-17', 'time': '03:15:42'})
         self.assertTrue(output == {'op': 'predict', 'body': 'unknown', 'date': '2016-01-17', 'time': '03:15:42', 'error': 'star not in catalog'})
+
+    def test_400_002(self):
+        output = SD.dispatch({'op': 'predict'})
+        self.assertTrue(output == {'op': 'predict', 'error': 'Mandatory information is missing'})
