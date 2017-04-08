043ff6b18b9d7dfad76608202ed49fa999b861f4
LinyuanZhang <lzz0035@auburn.edu>
2017-04-07 21:20:44 -0500

    Green Light |

--- softwareprocess/newpredict.py
+++ softwareprocess/newpredict.py
@@ -8,3 +8,13 @@ def Predict(values):
         values['error'] = 'star not in catalog'
         return values
     #star = StarsTable.getStarData(starName).split(',')
+
+    if 'body' not in values:
+        values['error'] = 'Mandatory information is missing'
+        return values
+    if 'lat' in values:
+        values['error'] = 'Latitude is invalid'
+        return values
+    if 'long' in values:
+        values['error'] = 'Longitude is invalid'
+        return values
