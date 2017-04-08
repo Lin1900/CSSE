58c4538ff78498204549ea0fd5f7f7f35d28c803
LinyuanZhang <lzz0035@auburn.edu>
2017-04-07 20:23:20 -0500

    Green Light |

--- softwareprocess/test/dispatch_Test.py
+++ softwareprocess/test/dispatch_Test.py
@@ -109,11 +109,11 @@ class DispatchTest(unittest.TestCase):
         output = SD.dispatch({'op': 'predict','body': 'abc', 'date': '2016-01-17', 'time': '03:15:42'})
         self.assertTrue(output == {'op': 'predict', 'body': 'abc', 'date': '2016-01-17', 'time': '03:15:42', 'error': 'star not in catalog'})
 
-    def test_400_003(self):
+    def test_400_002(self):
         output = SD.dispatch({'op': 'predict','body': 'Betelgeuse', 'date': '2016-01-17', 'time': '03:15:42'})
         self.assertTrue(output == {'op': 'predict', 'body': 'Betelgeuse', 'date': '2016-01-17', 'time': '03:15:42', 'long': '75d53.6', 'lat': '7d24.3'})
 
-    def test_400_002(self):
+    def test_400_003(self):
         output = SD.dispatch({'op': 'predict'})
         self.assertTrue(output == {'op': 'predict', 'error': 'Mandatory information is missing'})
 
@@ -122,18 +122,34 @@ class DispatchTest(unittest.TestCase):
         output = SD.dispatch({'op': 'predict','body': 'Betelgeuse', 'date': '2016-13-17', 'time': '03:15:42'})
         self.assertTrue(output == {'op': 'predict', 'body': 'Betelgeuse', 'date': '2016-13-17', 'time': '03:15:42', 'error': 'date is invalid'})
 
-    def test_400_004(self):
+    def test_400_005(self):
         output = SD.dispatch({'op': 'predict','body': 'Betelgeuse', 'date': '2016-11-31', 'time': '03:15:42'})
         self.assertTrue(output == {'op': 'predict', 'body': 'Betelgeuse', 'date': '2016-13-17', 'time': '03:15:42', 'error': 'date is invalid'})
 
-    def test_400_004(self):
+    def test_400_006(self):
         output = SD.dispatch({'op': 'predict','body': 'Betelgeuse', 'date': '2014-2-29', 'time': '03:15:42'})
-        self.assertTrue(output == {'op': 'predict', 'body': 'Betelgeuse', 'date': '2016-13-17', 'time': '03:15:42', 'error': 'date is invalid'})
+        self.assertTrue(output == {'op': 'predict', 'body': 'Betelgeuse', 'date': '2014-2-29', 'time': '03:15:42', 'error': 'date is invalid'})
 
-    def test_400_004(self):
+    def test_400_007(self):
         output = SD.dispatch({'op': 'predict','body': 'Betelgeuse', 'date': '2012-2-30', 'time': '03:15:42'})
-        self.assertTrue(output == {'op': 'predict', 'body': 'Betelgeuse', 'date': '2016-13-17', 'time': '03:15:42', 'error': 'date is invalid'})
+        self.assertTrue(output == {'op': 'predict', 'body': 'Betelgeuse', 'date': '2012-2-30', 'time': '03:15:42', 'error': 'date is invalid'})
 
-    def test_400_004(self):
-        output = SD.dispatch({'op': 'predict','body': 'Betelgeuse', 'date': '2003-4-32', 'time': '03:15:42'})
-        self.assertTrue(output == {'op': 'predict', 'body': 'Betelgeuse', 'date': '2003-4-32', 'time': '03:15:42', 'error': 'date is invalid'})
+    def test_400_008(self):
+        output = SD.dispatch({'op': 'predict','body': 'Betelgeuse', 'date': '2003-4-20', 'time': '33:15:42'})
+        self.assertTrue(output == {'op': 'predict', 'body': 'Betelgeuse', 'date': '2003-4-20', 'time': '33:15:42', 'error': 'time is invalid'})
+
+    def test_400_009(self):
+        output = SD.dispatch({'op': 'predict','body': 'Betelgeuse', 'date': '2016-11-30', 'time': '03:60:42'})
+        self.assertTrue(output == {'op': 'predict', 'body': 'Betelgeuse', 'date': '2016-11-30', 'time': '03:60:42', 'error': 'time is invalid'})
+
+    def test_400_010(self):
+        output = SD.dispatch({'op': 'predict','body': 'Betelgeuse', 'date': '2014-2-21', 'time': '03:15:60'})
+        self.assertTrue(output == {'op': 'predict', 'body': 'Betelgeuse', 'date': '2014-2-21', 'time': '03:15:60', 'error': 'time is invalid'})
+
+    def test_400_011(self):
+        output = SD.dispatch({'op': 'predict','body': 'Betelgeuse', 'date': '2000-2-17', 'time': '03:15:42'})
+        self.assertTrue(output == {'op': 'predict', 'body': 'Betelgeuse', 'date': '2000-2-17', 'time': '03:15:42', 'error': 'date is invalid'})
+
+    def test_400_012(self):
+        output = SD.dispatch({'op': 'predict','body': 'Betelgeuse', 'date': '2003-4-12', 'time': '3:15:42'})
+        self.assertTrue(output == {'op': 'predict', 'body': 'Betelgeuse', 'date': '2003-4-12', 'time': '3:15:42', 'error': 'time is invalid'})
