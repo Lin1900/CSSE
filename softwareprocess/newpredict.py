5056a32599aaa5434b6f08c872a7be177aaa7e70
LinyuanZhang <lzz0035@auburn.edu>
2017-04-07 21:54:24 -0500

    Red Light |

--- softwareprocess/newpredict.py
+++ softwareprocess/newpredict.py
@@ -23,8 +23,39 @@ def Predict(values):
         values['date'] = '2001-01-01'
     else:
         date = values['date'].split('-')
-        if len(date)
+        if len(date) != 3:
+            values['error'] = 'date is invalid'
+            return values
+        if checkDate(date) == -1:
+            values['error'] = 'date is invalid'
+            return values
+
+
 
 def checkDate(dates):
-    if len(dates) != 3:
+    year = dates[0]    #check year
+    if not year.isdigit() or len(year) != 4:
         return -1
+    if int(year) < 2001:
+        return -1
+    month = dates[1]   #check month
+    if not month.isdigit() or len(month) != 2:
+        return -1
+    if int(month) <= 0 or int(month) > 12:
+        return -1
+    day = dates[2]     #check day
+    if not day.isdigit() or len(day) != 2:
+        return -1
+    day = int(day)
+    if month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
+        if day != 31:
+           return -1
+    if month == 2 and year % 4 == 0:     #check leap year
+        if day != 29:
+            return -1
+    if month == 02 and year % 4 == 0:
+        if day != 28:
+            return -1
+    if month == 4 or month == 6 or month == 9 or month == 11:
+        if day != 30:
+            return -1
