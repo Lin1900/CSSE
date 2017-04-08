2326567555faabce3d286fa2ab71dc790eb7401e
LinyuanZhang <lzz0035@auburn.edu>
2017-04-08 01:09:19 -0500

    Green Light |

--- softwareprocess/newpredict.py
+++ softwareprocess/newpredict.py
@@ -101,7 +101,7 @@ def checkDate(dates):
     if not day.isdigit() or len(day) != 2:
         return -1
     day = int(day)
-    if month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
+    if month == 01 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
         if day != 31:
            return -1
     if month == 2 and year % 4 == 0:     #check leap year
