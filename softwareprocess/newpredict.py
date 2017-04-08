13d6e3bd428b19114d37303f7e0da803c29cd5da
LinyuanZhang <lzz0035@auburn.edu>
2017-04-07 22:05:18 -0500

    Green Light |

--- softwareprocess/newpredict.py
+++ softwareprocess/newpredict.py
@@ -2,6 +2,9 @@ import math
 import StarsTable
 
 def Predict(values):
+    if 'body' not in values:
+        values['error'] = 'Mandatory information is missing'
+        return values
 
     starName = values['body'][0].upper() + values['body'][1:].lower()
     if starName not in StarsTable.stars:
@@ -9,9 +12,6 @@ def Predict(values):
         return values
     #star = StarsTable.getStarData(starName).split(',')
 
-    if 'body' not in values:
-        values['error'] = 'Mandatory information is missing'
-        return values
     if 'lat' in values:
         values['error'] = 'Latitude is invalid'
         return values
