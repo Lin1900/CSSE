d0e9760d73184c1a6ac197080c216e99021891a8
LinyuanZhang <lzz0035@auburn.edu>
2017-04-07 22:05:22 -0500

    Green Light |

--- softwareprocess/test/dispatch_Test.py
+++ softwareprocess/test/dispatch_Test.py
@@ -119,8 +119,8 @@ class DispatchTest(unittest.TestCase):
 
 
     def test_400_004(self):
-        output = SD.dispatch({'op': 'predict', 'body': 'Betelgeuse', 'date': '2016-11-17', 'time': '33:15:42'})
-        self.assertTrue(output == {'op': 'predict', 'body': 'Betelgeuse', 'date': '2016-11-17', 'time': '33:15:42', 'error': 'time is invalid'})
+        output = SD.dispatch({'op': 'predict', 'body': 'Betelgeuse', 'date': '2016-11-17', 'time': '24:15:42'})
+        self.assertTrue(output == {'op': 'predict', 'body': 'Betelgeuse', 'date': '2016-11-17', 'time': '24:15:42', 'error': 'time is invalid'})
 
 
 """
