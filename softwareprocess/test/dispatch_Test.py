d4b0279b1d9c55df3292fc3522179a7083ab333f
LinyuanZhang <lzz0035@auburn.edu>
2017-04-27 22:16:13 -0500

    Green Light |

--- softwareprocess/test/dispatch_Test.py
+++ softwareprocess/test/dispatch_Test.py
@@ -5,7 +5,7 @@ import math
 class DispatchTest(unittest.TestCase):
 
     def test6_100_001_caculate(self):
-        output = SD.dispatch({'op':'correct', 'lat':'16d32.3', 'long':'95d41.6', 'altitude':'13d42.3',  'assumedLat':'-53d38.4', 'assumedLong':' 74d35.3'})
+        output = SD.dispatch({'op': 'correct', 'lat': '16d32.3', 'long':'95d41.6', 'altitude': '13d42.3', 'assumedLat': '-53d38.4', 'assumedLong': ' 74d35.3'})
         self.assertDictEqual(output, {'op':'correct', 'lat':'16d32.3', 'long':'95d41.6', 'altitude':'13d42.3',  'assumedLat':'-53d38.4', 'assumedLong':' 74d35.3', 'correctedDistance':'3950', 'correctedAzimuth':'164d43.1'})
 
 
