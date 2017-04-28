888ba69664854ebe0d3bbbcea026a551a38f5589
LinyuanZhang <lzz0035@auburn.edu>
2017-04-28 00:54:32 -0500

    Green Light |

--- softwareprocess/test/dispatch_Test.py
+++ softwareprocess/test/dispatch_Test.py
@@ -37,8 +37,8 @@ class DispatchTest(unittest.TestCase):
         self.assertDictEqual(output, {'op':'correct', 'lat': '16d32.3', 'long': '95d41.6', 'altitude':'13d42.3', 'assumedLat': '91d38.4', 'assumedLong': '74d35.3', 'error': 'assumedLat is invalid'})
 
     def test6_200_008_mis(self):
-        output = SD.dispatch({'op': 'correct', 'lat': '16d32.3', 'long': '95d41.6', 'altitude': '13d42.3', 'assumedLat': '-53d38.4', 'assumedLong': '74d35.3'})
-        self.assertDictEqual(output, {'op':'correct', 'lat': '16d32.3', 'long': '95d41.6', 'altitude':'13d42.3', 'assumedLat': '-53d38.4', 'assumedLong': '74d35.3', 'error': 'assumedLong is invalid'})
+        output = SD.dispatch({'op': 'correct', 'lat': '16d32.3', 'long': '95d41.6', 'altitude': '13d42.3', 'assumedLat': '-53d38.4', 'assumedLong': '74d-15.3'})
+        self.assertDictEqual(output, {'op':'correct', 'lat': '16d32.3', 'long': '95d41.6', 'altitude':'13d42.3', 'assumedLat': '-53d38.4', 'assumedLong': '74d-15.3', 'error': 'assumedLong is invalid'})
 
     def test6_200_009_mis(self):
         output = SD.dispatch({'op': 'correct', 'lat': '16d32.3', 'long': '95d41.6', 'altitude': '13d42.3', 'assumedLat': '2.38.4', 'assumedLong': '74d35.3'})
