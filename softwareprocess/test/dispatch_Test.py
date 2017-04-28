5926749216a0a4d4848a74639c52ebe87a57a7ac
LinyuanZhang <lzz0035@auburn.edu>
2017-04-28 00:54:06 -0500

    Green Light |

--- softwareprocess/test/dispatch_Test.py
+++ softwareprocess/test/dispatch_Test.py
@@ -33,12 +33,12 @@ class DispatchTest(unittest.TestCase):
         self.assertDictEqual(output, {'op':'correct', 'lat': '16d32.3', 'long': '95d41.6', 'altitude':'13d42.3', 'assumedLat': 42, 'assumedLong': '74d35.3', 'error': 'input is invalid'})
 
     def test6_200_007_mis(self):
-        output = SD.dispatch({'op': 'correct', 'lat': '16d32.3', 'long': '95d41.6', 'altitude': '13d42.3', 'assumedLat': '2ad38.4', 'assumedLong': '74d35.3'})
-        self.assertDictEqual(output, {'op':'correct', 'lat': '16d32.3', 'long': '95d41.6', 'altitude':'13d42.3', 'assumedLat': '2ad38.4', 'assumedLong': '74d35.3', 'error': 'assumedLat is invalid'})
+        output = SD.dispatch({'op': 'correct', 'lat': '16d32.3', 'long': '95d41.6', 'altitude': '13d42.3', 'assumedLat': '91d38.4', 'assumedLong': '74d35.3'})
+        self.assertDictEqual(output, {'op':'correct', 'lat': '16d32.3', 'long': '95d41.6', 'altitude':'13d42.3', 'assumedLat': '91d38.4', 'assumedLong': '74d35.3', 'error': 'assumedLat is invalid'})
 
     def test6_200_008_mis(self):
-        output = SD.dispatch({'op': 'correct', 'lat': '16d32.3', 'long': '95d41.6', 'altitude': '13d42.3', 'assumedLat': '-53d38.4', 'assumedLong': '74d-1'})
-        self.assertDictEqual(output, {'op':'correct', 'lat': '16d32.3', 'long': '95d41.6', 'altitude':'13d42.3', 'assumedLat': '-53d38.4', 'assumedLong': '74d-1', 'error': 'assumedLong is invalid'})
+        output = SD.dispatch({'op': 'correct', 'lat': '16d32.3', 'long': '95d41.6', 'altitude': '13d42.3', 'assumedLat': '-53d38.4', 'assumedLong': '74d35.3'})
+        self.assertDictEqual(output, {'op':'correct', 'lat': '16d32.3', 'long': '95d41.6', 'altitude':'13d42.3', 'assumedLat': '-53d38.4', 'assumedLong': '74d35.3', 'error': 'assumedLong is invalid'})
 
     def test6_200_009_mis(self):
         output = SD.dispatch({'op': 'correct', 'lat': '16d32.3', 'long': '95d41.6', 'altitude': '13d42.3', 'assumedLat': '2.38.4', 'assumedLong': '74d35.3'})
